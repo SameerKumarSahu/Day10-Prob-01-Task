e4360d8a202c483687affc85782489deb9a476e2
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-26 00:40:09 +0530

    Take 3 input value

--- SortingAlgorithum.sh
+++ SortingAlgorithum.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "Welcome to the Program"
+
+
+
+read -p "Enter the First number :-" a
+echo "the First number is " $a
+
+
+read -p "Enter the Second number :-" b
+echo "the Second number is " $b
+
+
+read -p "Enter the Third number :- " c
+echo "the Third number is " $c
